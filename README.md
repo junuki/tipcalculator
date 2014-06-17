3e239e5a7884a271b73c006f8693bbedb779901a
junuki <jungkim@jungkim-macbookpro2.roam.corp.google.com>
2014-06-17 07:03:04 -0700

    Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+tipcalculator
+=============
+learning iOS
+this is minor change 
+
+this is 2nd attempt of minor change
+
+this is 3rd attempt of minor change
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jung Eun Kim on 6/17/14.
+//  Copyright (c) 2014 jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
