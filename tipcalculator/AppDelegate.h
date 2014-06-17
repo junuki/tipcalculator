0cbd041f47f44a7b89f12d76d18fb2deb9dee23f
Jung Eun Kim <jungkim@jungkim-macbookpro2.roam.corp.google.com>
2014-06-17 05:15:23 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Jung Eun Kim on 6/17/14.
+//  Copyright (c) 2014 jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
